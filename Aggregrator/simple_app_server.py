b18f5a77adbba8c94d3e2a61310af0faf84dd6ff
Hollis <86034669+Hollis-Chen@users.noreply.github.com>
2023-12-21 04:51:42 -0500

    Add files via upload

--- Aggregrator/simple_app_server.py
+++ Aggregrator/simple_app_server.py
@@ -1,23 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse, HTMLResponse
 from pydantic import BaseModel
 import asyncio
 import uvicorn
+import aiohttp
+
 # from resources.student_resource import StudentResource, Student
 
 # from resources.ScholarNest_resource import Resource, user
-from resources.ScholarNest_resource import scholarResource,user
+from resources.ScholarNest_resource import ScholarResource,User
 
 
 app = FastAPI()
 
-# ##########################
-#### my testbench
-######################################
 
-# example_instance = StudentResource()
-# example_instance = Resource()
-example_instance = scholarResource()
+#
+# #################
+# #####ScholarResouece:
+# ######################
+scholar_resource_instance = ScholarResource()
 
 
 
@@ -53,154 +54,53 @@ async def home_page():
     return HTMLResponse(home_page)
 
 
-@app.get("/get_item")
-async def async_call(item_name: str = None):
-    result = await example_instance.get_item(item_name)
-    return JSONResponse(content={"message": result})
 
 
-# @app.get("/get_student_async")
-@app.get("/get_scholarnest_async")
-async def async_call():
-    result = await example_instance.get_scholarnest_async()
-    return JSONResponse(content={"result": result})
+@app.post("/aggregate_data")
+async def aggregate_data(user_id: str):
+    try:
+        user_group = await scholar_resource_instance.allocate_group(user_id)
+        async with aiohttp.ClientSession() as session:
+            papers = await scholar_resource_instance.fetch_papers(session, user_group["id"])
+        return {"group": user_group, "papers": papers}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-# @app.get("/get_student_sync")
-@app.get("/get_scholarnestnest_sync")
-async def async_call():
-    result_sync = await example_instance.get_scholarnest_sync()
-    return JSONResponse(content={"result_sync": result_sync})
+
+@app.get("/user_info/{user_id}")
+async def get_user_info(user_id: str):
+    try:
+        user_group = await scholar_resource_instance.id_check(user_id)
+        print(user_group)
+
+        # Check if the user is not grouped
+        if "message" in user_group and user_group["message"] == "User not grouped":
+            return user_group
+
+        async with aiohttp.ClientSession() as session:
+            papers = await scholar_resource_instance.fetch_papers(session, user_group["id"])
+        return {"group": user_group, "papers": papers}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/user_papers/{user_id}")
+async def get_user_paper_info(user_id: str):
+    try:
+
+
+        async with aiohttp.ClientSession() as session:
+            papers = await scholar_resource_instance.fetch_personal_papers(session)
+        return {"papers": papers}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
-##########################################################################
+#########################################################################
 
 
-
-# ###################################################
-#
-# ##########   simple example
-# #############################################
-#
-# app = FastAPI()
-# scholarNest_instance = ScholarNestResource()
-#
-# @app.get("/")
-# async def home_page():
-#     home_page_content = """
-#     <!DOCTYPE html>
-#     <html lang="en">
-#     <head>
-#         <meta charset="UTF-8">
-#         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#         <title>ScholarNest Aggregator Service</title>
-#     </head>
-#     <body>
-#         <header>
-#             <h1>Welcome to ScholarNest Aggregator Service</h1>
-#         </header>
-#         <section>
-#             <h2>Usage</h2>
-#             <p>Please go to <a href="/docs">the OpenAPI docs page for this app.</a>
-#         </section>
-#         <footer>
-#             <p>&copy; 2023 ScholarNest. All rights reserved.</p>
-#         </footer>
-#     </body>
-#     </html>
-#     """
-#     return HTMLResponse(home_page_content)
-#
-# @app.get("/aggregate_async")
-# async def aggregate_data_async():
-#     result = await scholarNest_instance.aggregate_data_async()
-#     return JSONResponse(content=result)
-#
-# @app.get("/aggregate_sync")
-# def aggregate_data_sync():
-#     result = scholarNest_instance.aggregate_data_sync()
-#     return JSONResponse(content=result)
-#
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
-#
-# # ###################################################################
-
-
-
-
-# ############################
-########## working class example
-###################################
-# from fastapi import FastAPI
-# from fastapi.responses import JSONResponse, HTMLResponse
-# from pydantic import BaseModel
-# import asyncio
-# import uvicorn
-# from resources.student_resource import StudentResource, Student
-#
-# app = FastAPI()
-#
-# example_instance = StudentResource()
-#
-#
-# @app.get("/")
-# async def home_page():
-#     home_page = \
-#         """
-#         <!DOCTYPE html>
-#         <html lang="en">
-#         <head>
-#             <meta charset="UTF-8">
-#             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#             <title>Simple Composite Service Example</title>
-#         </head>
-#         <body>
-#
-#             <header>
-#                 <h1>Welcome to Simple Composite Example</h1>
-#             </header>
-#
-#             <section>
-#                 <h2>Usage</h2>
-#                 <p>Please go to <a href="/docs">the OpenAPI docs page for this app.</a>
-#             </section>
-#
-#             <footer>
-#                 <p>&copy; 2023 Donald Ferguson. All rights reserved.</p>
-#             </footer>
-#
-#         </body>
-#         </html>
-#         """
-#     return HTMLResponse(home_page)
-#
-#
-# @app.get("/get_item")
-# async def async_call(item_name: str = None):
-#     result = await example_instance.get_item(item_name)
-#     return JSONResponse(content={"message": result})
-#
-#
-# @app.get("/get_student_async")
-# async def async_call():
-#     result = await example_instance.get_student_async()
-#     return JSONResponse(content={"students": result})
-#
-#
-# @app.get("/get_student_sync")
-# async def async_call():
-#     result = await example_instance.get_student_sync()
-#     return JSONResponse(content={"students": result})
-#
-#
-# if __name__ == "__main__":
-#     import uvicorn
-#
-#     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
-#
-##########################################################
